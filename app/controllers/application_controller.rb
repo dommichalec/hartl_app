2311aac5042ad68aac2de30ccc48ae278b337605
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-09-03 22:33:53 -0400

    add static pages controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # test hello as root to make sure Heroku deployment is successful
   def hello
     render html: "Hello, world"
   end
