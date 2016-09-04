27111d9429f80534acfab3e63c35b67f3e365ecf
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-09-04 15:06:46 -0400

    update full_name helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,11 @@ module ApplicationHelper
 
   # renders the full title via <% provide(:title, "Something") %>
   def full_title(title)
-    "#{title} | Ruby on Rails Tutorial"
+    base_title = "Ruby on Rails Tutorial"
+    if title.empty?
+      base_title
+    else
+      "#{title} | Ruby on Rails Tutorial"
+    end
   end
 end
