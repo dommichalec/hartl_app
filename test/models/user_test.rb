b9a5755bab59731054739fc980c88eb84ffde483
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-09-05 16:51:39 -0400

    begin user model unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(first_name: "Sample", last_name: "User",
+                    email: "sampleuser@example.com")
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
 end
