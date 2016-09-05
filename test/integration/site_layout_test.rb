20fc55fd5434347f0c51b8dfdef97bbe14908499
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-09-05 15:01:56 -0400

    start Users controller

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", helf_path, count: 1
     assert_select "a[href=?]", about_path, count: 1
     assert_select "a[href=?]", contact_path, count: 1
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
