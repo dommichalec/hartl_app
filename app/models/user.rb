0fd70b429f98af2f5ae788caff588c0815de5564
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-09-11 09:47:57 -0400

    update user attributes to include has_secure_password and password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  # needed for bcrypt
+  has_secure_password # attr_accessor :password, :password_confirmation
+  # callbacks
+  before_save { self.email = email.downcase }
+  before_save { self.first_name = first_name.capitalize}
+  before_save { self.last_name = last_name.capitalize}
+  # constants
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   # validations
-  validates :first_name, :last_name, presence: true, length: { minimum: 2 }
+  validates(:first_name, :last_name, { presence: true, length: { in: 2..50 }})
+  validates(:email, { presence: true , length: { maximum: 50 }, format:
+  { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }})
+  validates(:password, presence: true, length: { minimum: 6 })
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,8 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(first_name: "Sample", last_name: "User",
-                    email: "sampleuser@example.com")
+                    email: "sampleuser@example.com", password: "foobar",
+                    password_confirmation: "foobar")
   end
 
   test "user should be valid" do
@@ -20,6 +21,55 @@ class UserTest < ActiveSupport::TestCase
   test "user first name and last name should be longer than 1 character" do
     @user.first_name = "a"
     @user.last_name = "b"
+    assert_not @user.valid?
+  end
+
+  test "user first name and last name shouldn't be longer than 50 characters" do
+    @user.first_name = "a"*51
+    @user.last_name = "b"*51
+    assert_not @user.valid?
+  end
+
+  test "user email address should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "user email address should not be longer than 50 characters" do
+    @user.email = "a"*51
+    assert_not @user.valid?
+  end
+
+  test "user email validation should accept valid addresses" do
+   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+   valid_addresses.each do |valid_address|
+     @user.email = valid_address
+     assert @user.valid?, "#{valid_address.inspect} should be valid"
+   end
+  end
+
+  test "user email address should be unique to the user" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should have a minimum length of 6 characters" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 end
