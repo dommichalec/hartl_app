f82b394ce4c8e767085483c21cf93741ad514849
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-09-11 12:24:50 -0400

    add format to rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   # use fail to dump params hash to the browser
 
   def show
-    @user = User.find(params[:id])
+    respond_to do |format|
+      format.html { @user = User.find(params[:id]) }
+    end
   end
 
   def new
